5feb22eb8227f73c4a7871d669bfa2a835cabf65
NICHOLAST0RRES <nicholas.lima.torres@gmail.com>
2026-03-15 18:15:42 -0300

    teste do n8n 8

--- src/main/java/com/paguepouco/api/dtos/Farmaceutico/CadastroFarmaceutico.java
+++ src/main/java/com/paguepouco/api/dtos/Farmaceutico/CadastroFarmaceutico.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 
 
 public record CadastroFarmaceutico(@NotBlank(message = "ERRO, O NOME DO FARMACEUTICO NÃO PODE SER CADASTRADO EM BRANCO !!!!!") String nome,
-                                   @NotBlank(message = "ERRO, O TIPO DO FARMACEUTICO NÃO PODE SER CADASTRADO EM BRANCO !!!") String tipo,
-                                   @NotBlank(message = "ERRO, O CRF DO FARMACEUTICO NÃO PODE SER CADASTRADO EM BRANCO !!!") String crf ,
+                                   @NotBlank(message = "ERRO, O TIPO DO FARMACEUTICO NÃO PODE SER CADASTRADO EM BRANCO !!!!") String tipo,
+                                   @NotBlank(message = "ERRO, O CRF DO FARMACEUTICO NÃO PODE SER CADASTRADO EM BRANCO !!!!") String crf ,
                                     Especialidade especialidade ,
-                                   @NotBlank(message = "ERRO, O TURNO DO FARMACEUTICO NÃO PODE SER CADASTRADO EM BRANCO !!!") String turno) {
+                                   @NotBlank(message = "ERRO, O TURNO DO FARMACEUTICO NÃO PODE SER CADASTRADO EM BRANCO !!!!") String turno) {
 }
